7a3a612857ee64ad6e8c648d205bd348727ea1e2
Denis Verstraeten <lepilotedef22@gmail.com>
2022-12-23 00:10:57 +0100

    dl + files

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 
 check_install_package () {
     if [ $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    # Credit: https://stackoverflow.com/questions/1298066/how-can-i-check-if-a-package-is-installed-and-install-it-if-not
     then
         sudo apt install -y $1;
     fi
@@ -15,3 +16,16 @@ echo "Checking if required packages are installed..."
 check_install_package wget
 check_install_package npm
 check_install_package sqlite3
+
+echo "Downloading installation files for Wiki.js..."
+wget https://github.com/Requarks/wiki/releases/latest/download/wiki-js.tar.gz
+
+echo "Decompressing the archive..."
+mkdir wiki
+tar xzf wiki-js.tar.gz -C ./wiki_temp
+cd wiki_temp
+
+echo " Creating the directories..."
+sudo mkdir /var/wiki/
+sudo mkdir /var/wiki/sqlite
+npm rebuild sqlite3
